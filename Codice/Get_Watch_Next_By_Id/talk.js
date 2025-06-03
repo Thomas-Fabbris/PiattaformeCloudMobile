ac7459c05f5395ac7c99014f8f1112412cd75a64
Thomas Fabbris <115624667+Thomas-Fabbris@users.noreply.github.com>
2025-06-03 19:09:58 +0200

    Update talk.js

--- Codice/Get_Watch_Next_By_Id/talk.js
+++ Codice/Get_Watch_Next_By_Id/talk.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 /**
  * URL Validator Function
  * @param {string} url - The URL string to validate.
@@ -16,27 +15,31 @@ const urlValidator = function(url) {
     return false;
   }
 };
+
 const talk_schema = new mongoose.Schema({
-    _id:{
-        type:String,
-        required: [true, 'id is required'],
-        unique:true
-    }, 
-    title: String,
-    url: {
-        type:String,
-        required:true,
-        required: [true, 'URL is required.'],
-        validate: {
-          validator: urlValidator,
-          message: props => `${props.value} is not a valid website URL. Please ensure it starts with http:// or https://`
-        }
-    },
-    description: String,
-    duration:Int32Array,
-    publishedAt: Date,
-    speakers: String,
-    tags:[String]
- }, {collection: 'tedx_data_cleaned' });
+  _id: {
+    type: String,
+    required: [true, 'id is required']
+  },
+  title: String,
+  url: {
+    type: String,
+    required: [true, 'URL is required.'],
+    validate: {
+      validator: urlValidator,
+      message: props => `${props.value} is not a valid website URL. Please ensure it starts with http:// or https://`
+    }
+  },
+  description: String,
+  duration: {
+    type: Number
+  },
+  publishedAt: Date,
+  speakers: String,
+  tags: [String],
+  related_video_ids: [String] 
+}, { 
+  collection: 'tedx_data_cleaned'
+});
 
 module.exports = mongoose.model('talk', talk_schema);
