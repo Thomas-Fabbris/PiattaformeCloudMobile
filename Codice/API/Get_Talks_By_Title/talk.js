6b87ce0b065f1e1fb614f4fb59676269be449f1d
Thomas Fabbris <115624667+Thomas-Fabbris@users.noreply.github.com>
2025-06-09 14:48:54 +0200

    Update talk.js

--- Codice/API/Get_Talks_By_Title/talk.js
+++ Codice/API/Get_Talks_By_Title/talk.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const talk_schema = new mongoose.Schema({
+    _id: String,
     title: String,
     url: String,
     description: String,
     speakers: String,
     comprehend_analysis: mongoose.Schema.Types.Mixed
-}, { collection: 'tedx_data' });
+}, { collection: 'tedx_data_cleaned' });
 
 module.exports = mongoose.model('talk', talk_schema);
